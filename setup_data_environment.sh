ba3a1156e0f70996f254d680054cee4a3bcc4e84
mattersoflight <shalin.mehta@czbiohub.org>
2021-09-26 22:13:16 +0000

    update setup script

--- setup_data_environment.sh
+++ setup_data_environment.sh
@@ -4,16 +4,22 @@
 cd 
 
 # copy the data to the home directory:
+echo -e "transferring data:\n"
 rsync -r --info=progress2 --info=name0 /mnt/efs/woods_hole/04_image_translation_data ~
 
 # Download `microDL` repository, and checkout the dl_mbl_2021 branch.
+
+echo -e "setup the microDL repo:\n"
 git clone https://github.com/czbiohub/microDL.git
-cd microDL
+cd ~/microDL
 git checkout dl_mbl_2021
 
-# create conda environment, activate it, and add the module to python path.
+# create conda environment and add the module to python path.
+
+echo -e "setup the environment:\n"
 conda env create --file=conda_environment.yml
-conda activate micro_dl
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
+# change back to home directory.
+cd
 
