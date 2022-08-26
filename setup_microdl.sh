908480fca100e49f91add85b3fbfe956d6f1ed02
mattersOfLight <shalin.mehta@gmail.com>
2022-08-26 21:27:31 +0000

    edit exercise and solution

--- setup_microdl.sh
+++ setup_microdl.sh
@@ -6,12 +6,9 @@ echo -e "setup the microDL repo:\n"
 git clone https://github.com/mehta-lab/microDL.git
 cd microDL
 git fetch --all --tags
-git checkout tags/v1.0.0-rc1
+git checkout tags/v1.0.0-rc2
 
-# create conda environment and add the module to python path.
-
-echo -e "setup the environment:\n"
-conda env create --file=conda_env_microdl_care.yml
+# add microDL to pythonpath
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 # change back to the parent directory.
