f0621877353fd8212472b144b9f14b64c6f8fc18
Shalin Mehta <shalin.mehta@gmail.com>
2023-08-22 17:39:41 +0000

    make setup more robust

--- setup.sh
+++ setup.sh
@@ -4,17 +4,19 @@
 mamba create --name 04_image_translation python=3.10
 
 # Install ipykernel in the environment.
-mamba install -y ipykernel --name 04_image_translation
+mamba install -y ipykernel nbformat nbconvert black jupytext --name 04_image_translation
 # Specifying the environment explicitly.
 # mamba activate sometimes doesn't work from within shell scripts.
 
-
-# Find path to the environment - mamba activate doesn't work from within shell scripts.
-ENV_PATH=$(conda info --envs | grep 04_image_translation | awk '{print $2}')
 # install viscy and its dependencies in the environment using pip.
-$ENV_PATH/bin/pip install "viscy[metrics] @ git+https://github.com/mehta-lab/viscy.git@dlmbl2023"
-
-
+mkdir -p ~/code/
+cd ~/code/
+git clone https://github.com/mehta-lab/viscy.git
+cd viscy
+git checkout dlmbl2023
+# Find path to the environment - mamba activate doesn't work from within shell scripts.
+ENV_PATH=$(conda info --envs | grep 04_image_translation | awk '{print $NF}')
+$ENV_PATH/bin/pip install ."[metrics]"
 # Store the code directory path.
 CODE_DIR=$(pwd)
 
@@ -22,11 +24,10 @@ CODE_DIR=$(pwd)
 # Create data directory
 mkdir -p ~/data/04_image_translation
 cd ~/data/04_image_translation
-wget https://dl-at-mbl-2023-data.s3.us-east-2.amazonaws.com/DLMBL2023_image_translation_data.tar.gz
-tar -xzf DLMBL2023_image_translation_data.tar.gz 
-
-# # Download and unzip the data
-touch here_lives_the_data
+wget https://dl-at-mbl-2023-data.s3.us-east-2.amazonaws.com/DLMBL2023_image_translation_data_pyramid.tar.gz
+wget https://dl-at-mbl-2023-data.s3.us-east-2.amazonaws.com/DLMBL2023_image_translation_test.tar.gz 
+tar -xzf DLMBL2023_image_translation_data_pyramid.tar.gz 
+tar -xzf DLMBL2023_image_translation_test.tar.gz
 
 # Go back to the code directory
 cd $CODE_DIR
