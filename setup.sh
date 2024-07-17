296fcf3f711eb334eb42e8deed323707f7495d72
Tonks684 <smtonks2712@gmail.com>
2024-07-17 16:40:06 -0400

    setup changes

--- setup.sh
+++ setup.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env -S bash -i
-
 START_DIR=$(pwd)
-
-# Create mamba environment
-conda create -y --name 04_image_translation python=3.8
-
+mkdir -p ./GAN_code/
+cd ./GAN_code/
+git clone git@github.com:Tonks684/GANs_MI2I.git
+# Create conda environment from yml
+cd ./GANs_MI2I/
+conda env create -f ./pix2pixHD_CUDA11_environment.yml
+source activate pixpi2xHD_CUDA11
 # Install ipykernel in the environment.
-conda install -y ipykernel nbformat nbconvert black jupytext ipywidgets --name 04_image_translation
-# Specifying the environment explicitly.
-# mamba activate sometimes doesn't work from within shell scripts.
-
-# install viscy and its dependencies`s in the environment using pip.
-mkdir -p ~/GAN_code/
-git clone https://github.com/Tonks684/GANs_MI2I.git
-
-# Find path to the environment - mamba activate doesn't work from within shell scripts.
-ENV_PATH=$(conda info --envs | grep 04_image_translation | awk '{print $NF}')
-$ENV_PATH/bin/pip install ."[metrics]"
-
+conda install -y ipykernel nbformat nbconvert black jupytext ipywidgets --name pix2pixHD_CUDA11
+cd ../../
 # Create data directory
-output_dir = "~/data/04_image_translation/tiff_files/"
+output_dir = "./data/04_image_translation/tiff_files/"
 mkdir -p output_dir
 # Download Data
-cd "~/GAN_code/GANs_MI2I/"
+cd ./GAN_code/GANs_MI2I/
 python download_and_split_dataset.py --output_image_folder output_dir --crop_size 512
 
 # Change back to the starting directory
