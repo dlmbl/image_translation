e550fe7f02dc4c7dfd020602e0f3ad8754e87294
Tonks684 <smtonks2712@gmail.com>
2024-07-18 06:38:21 -0400

    setup fixes

--- setup.sh
+++ setup.sh
@@ -1,21 +1,36 @@
-#!/usr/bin/env -S bash -i
+#!/usr/bin/env bash
+
+# Save current directory
 START_DIR=$(pwd)
+
+# Create and navigate to GAN_code directory
 mkdir -p ./GAN_code/
 cd ./GAN_code/
+
+# Clone the Git repository
 git clone git@github.com:Tonks684/GANs_MI2I.git
+
 # Create conda environment from yml
 cd ./GANs_MI2I/
-conda env create -f ./pix2pixHD_CUDA11_environment.yml
-source activate pixpi2xHD_CUDA11
-# Install ipykernel in the environment.
-conda install -y ipykernel nbformat nbconvert black jupytext ipywidgets --name pix2pixHD_CUDA11
-cd ../../
-# Create data directory
-output_dir = "./data/04_image_translation/tiff_files/"
-mkdir -p output_dir
-# Download Data
-cd ./GAN_code/GANs_MI2I/
-python download_and_split_dataset.py --output_image_folder output_dir --crop_size 512
+conda env create -f ./pix2pixHDCUDA11_environment.yml
 
-# Change back to the starting directory
+# Activate the environment (fixed typo and deprecated usage)
+source activate pix2pixHD_CUDA11
+
+# Install additional packages
+conda install -y ipykernel nbformat nbconvert black jupytext ipywidgets --name pix2pixHD_CUDA11
+
+# Return to the previous directory
+cd ../../
+
+# Define and create the output directory (fixed variable assignment and usage)
+output_dir="./data/04_image_translation/tiff_files/"
+mkdir -p $output_dir
+
+# Download and split the dataset
+cd ./GAN_code/GANs_MI2I/
+python download_and_split_dataset.py --output_image_folder $output_dir --crop_size 512
+
+# Return to the starting directory
 cd $START_DIR
+
